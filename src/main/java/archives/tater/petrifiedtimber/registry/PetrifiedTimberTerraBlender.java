b522eb20eb969d923b3a5d877f65f48d7019e28c
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-18 13:39:17 -0700

    concentric

--- src/main/java/archives/tater/petrifiedtimber/registry/PetrifiedTimberTerraBlender.java
+++ src/main/java/archives/tater/petrifiedtimber/registry/PetrifiedTimberTerraBlender.java
@@ -4,57 +4,29 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.biome.Climate.Parameter;
 
-import terrablender.api.Region;
-import terrablender.api.RegionType;
-import terrablender.api.Regions;
-import terrablender.api.TerraBlenderApi;
+import terrablender.api.ParameterUtils.ParameterPointListBuilder;
+import terrablender.api.*;
 
 import java.util.function.Consumer;
 
 public class PetrifiedTimberTerraBlender implements TerraBlenderApi {
-
-    public static final Climate.Parameter FULL_RANGE = Climate.Parameter.span(-1f, 1f);
-
-    private final Climate.Parameter mushroomFieldsContinentalness = Climate.Parameter.span(-1.2F, -1.05F);
-    private final Climate.Parameter deepOceanContinentalness = Climate.Parameter.span(-1.05F, -0.455F);
-    private final Climate.Parameter oceanContinentalness = Climate.Parameter.span(-0.455F, -0.19F);
-    private final ResourceKey<Biome>[][] OCEANS = new ResourceKey[][]{
-            {Biomes.DEEP_FROZEN_OCEAN, Biomes.DEEP_COLD_OCEAN, Biomes.DEEP_OCEAN, Biomes.DEEP_LUKEWARM_OCEAN, Biomes.WARM_OCEAN},
-            {Biomes.FROZEN_OCEAN, Biomes.COLD_OCEAN, Biomes.OCEAN, Biomes.LUKEWARM_OCEAN, Biomes.WARM_OCEAN}
-    };
-    private final Climate.Parameter[] temperatures = new Climate.Parameter[]{
-            Climate.Parameter.span(-1.0F, -0.45F),
-            Climate.Parameter.span(-0.45F, -0.15F),
-            Climate.Parameter.span(-0.15F, 0.2F),
-            Climate.Parameter.span(0.2F, 0.55F),
-            Climate.Parameter.span(0.55F, 1.0F)
-    };
-
-    private static void addSurfaceBiome(
-            Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> consumer,
-            Climate.Parameter temperature,
-            Climate.Parameter humidity,
-            Climate.Parameter continentalness,
-            Climate.Parameter erosion,
-            Climate.Parameter depth,
-            float weirdness,
-            ResourceKey<Biome> key
-    ) {
-        consumer.accept(Pair.of(Climate.parameters(temperature, humidity, continentalness, erosion, Climate.Parameter.point(0.0F), depth, weirdness), key));
-        consumer.accept(Pair.of(Climate.parameters(temperature, humidity, continentalness, erosion, Climate.Parameter.point(1.0F), depth, weirdness), key));
-    }
 
     @Override
     public void onTerraBlenderInitialized() {
         Regions.register(new Region(PetrifiedTimberWorldgen.PETRIFIED_FOREST.identifier(), RegionType.OVERWORLD, 2) {
             @Override
             public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper) {
-                addModifiedVanillaOverworldBiomes(mapper, builder -> {
-                    builder.replaceBiome(Biomes.MUSHROOM_FIELDS, PetrifiedTimberWorldgen.PETRIFIED_FOREST);
-                });
+                var builder = new VanillaParameterOverlayBuilder();
+
+                new ParameterPointListBuilder()
+                        .continentalness(Parameter.span(-1.3F, -1.2F))
+                        .build()
+                        .forEach(point -> builder.add(point, PetrifiedTimberWorldgen.PETRIFIED_FOREST));
+
+                builder.build().forEach(mapper);
             }
         });
     }
